609a07771862316c068c864c49de521ed95b2fe9
esraozdem <esraozdem43@gmail.com>
2022-09-14 17:12:05 -0400

    updates

---
+++ index.js
@@ -0,0 +1,21 @@
+const acElement = document.querySelector(".ac");
+const pmElement = document.querySelector(".pm");
+const percentElement = document.querySelector(".percent");
+
+const additionEl = document.querySelector(".addition");
+const subsractionEl = document.querySelector(".substraction");
+const divisionEl = document.querySelector(".division");
+const multiplicationEl = document.querySelector(".multiplication");
+const equalEl = document.querySelector(".equal");
+
+const decimalEl = document.querySelector(".decimal");
+const number0El = document.querySelector(".number-0");
+const number1El = document.querySelector(".number-1");
+const number2El = document.querySelector(".number-2");
+const number3El = document.querySelector(".number-3");
+const number4El = document.querySelector(".number-4");
+const number5El = document.querySelector(".number-5");
+const number6El = document.querySelector(".number-6");
+const number7El = document.querySelector(".number-7");
+const number8El = document.querySelector(".number-8");
+const number9El = document.querySelector(".number-9");
